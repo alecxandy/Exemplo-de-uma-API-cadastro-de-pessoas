aaccb36eafcd7cb0473400c6114d3eeab1945530
alexandre gomes <alecxandy@gmail.com>
2022-06-12 14:15:31 -0300

    Initial commit

---
+++ src/main/java/com/alexandredvlp/Exemplo/de/uma/API/cadastro/de/pessoas/ExemploDeUmaApiCadastroDePessoasApplication.java
@@ -0,0 +1,13 @@
+package com.alexandredvlp.Exemplo.de.uma.API.cadastro.de.pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploDeUmaApiCadastroDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploDeUmaApiCadastroDePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandredvlp/Exemplo/de/uma/API/cadastro/de/pessoas/ExemploDeUmaApiCadastroDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandredvlp.Exemplo.de.uma.API.cadastro.de.pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploDeUmaApiCadastroDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
