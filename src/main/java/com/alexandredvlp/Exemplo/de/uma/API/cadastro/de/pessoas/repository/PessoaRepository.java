d96595466ac51ff85b1e7f15939f2db099555550
alexandre gomes <alecxandy@gmail.com>
2022-06-18 01:22:32 -0300

    consultas personalizadas

--- src/main/java/com/alexandredvlp/Exemplo/de/uma/API/cadastro/de/pessoas/repository/PessoaRepository.java
+++ src/main/java/com/alexandredvlp/Exemplo/de/uma/API/cadastro/de/pessoas/repository/PessoaRepository.java
@@ -5,11 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
-    //consulta personalizada
-    public List<Pessoa> findByName(String nome);
-    public List<Pessoa> findByEmail(String email);
+    //retorna uma lista de nomes
+    List<Pessoa> findByName(String nome);
+
+    //retorna uma lista de email
+    List<Pessoa> findByEmail(String email);
+
+    //retona o primeiro nome
+    Optional<Pessoa> findFirstName(String nome);
 }
